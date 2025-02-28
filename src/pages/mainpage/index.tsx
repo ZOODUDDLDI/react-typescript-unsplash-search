491c51d1ce49a16111323f08d04bd279f2df57f3
ZOODUDDLDI <wndud238561@gmail.com>
2025-02-28 11:28:14 +0900

    [feat] 기본 레이아웃 구조 설정 및 스타일 추가 (헤더, 네비게이션, 검색창 UI)

--- src/pages/mainpage/index.tsx
+++ src/pages/mainpage/index.tsx
@@ -1,8 +1,17 @@
+import styles from "./index.module.scss";
 
 function index() {
   return (
-    <div>메인페이지</div>
-  )
+    <div className={styles.page}>
+      {/* 공통 헤더 */}
+
+      {/* 공통 네비게이션 */}
+
+      {/* 검색창 UI 부분 */}
+
+      {/* 인터 */}
+    </div>
+  );
 }
 
-export default index
+export default index;
