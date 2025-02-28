b3dbe0ba081677e8fe634327278bb648ed1cdfdc
ZOODUDDLDI <wndud238561@gmail.com>
2025-02-28 09:33:59 +0900

    [fix]: React Strict Mode 해제 및 App.tsx 복구

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <div>안녕</div>
-  </StrictMode>
-);
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(<App />);
