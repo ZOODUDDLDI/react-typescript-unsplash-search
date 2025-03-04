d0975276f0b112e3d2926d8e2675d84ce1df77bf
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-04 22:08:41 +0900

    [delete] : api 삭제.

--- src/recoil/selectors/imageSelectors.ts
+++ src/recoil/selectors/imageSelectors.ts
@@ -7,7 +7,7 @@ import { searchState } from "../atoms/searchState";
 import { pageState } from "../atoms/pageState";
 
 const API_URL = "https://api.unsplash.com/search/photos";
-const API_KEY = "dOatUV3JMFQzqj-V1K5o5YVPyh08kXckcbWDRcVQgIk";
+const API_KEY = "";
 const PER_PAGE = 30;
 
 export const imageData = selector({
