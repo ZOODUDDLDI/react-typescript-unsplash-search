33f9a61b2483ae64752834807928b7768c2e5ecf
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-04 15:29:09 +0900

    [style]: 메뉴 클릭시, 선택시 스타일 변경.

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -17,7 +17,15 @@ function Navigation() {
 
   const navLinks = navigation.map((item: Navigation) => {
     return (
-      <Link to={item.path} className={styles.navigation__menu} key={item.path}>
+      <Link
+        to={item.path}
+        className={
+          item.isActive
+            ? `${styles.navigation__menu} ${styles.active}`
+            : `${styles.navigation__menu} ${styles.inactive}`
+        }
+        key={item.path}
+      >
         <span className={styles.navigation__menu__label}>{item.label}</span>
       </Link>
     );
