0e87903e69c7f575a507fd679b1eff635c0a428f
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-04 16:18:27 +0900

    [feat]: 메인페이지 레이아웃에 콘텐츠 섹션 및 스타일 추가.

--- src/pages/mainpage/index.tsx
+++ src/pages/mainpage/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./index.module.scss";
 
-import Header from "@components/header/Header";
+import Header from "@/components/header/Header";
 import Navigation from "@/components/navigation/Navigation";
 import SearchBar from "@/components/searchBar/SearchBar";
 
@@ -13,10 +13,24 @@ function index() {
       {/* 공통 네비게이션 */}
       <Navigation />
 
-      {/* 검색창 UI 부분 */}
-      <SearchBar />
+      {/* */}
+      <div className={styles.page__contents}>
+        <div className={styles.page__contents__introBox}>
+          <div className={styles.wrapper}>
+            <span className={styles.wrapper__title}>SnapSplash</span>
+            <span className={styles.wrapper__desc}>
+              손끝에서 펼쳐지는 아름다운 이미지의 세계, <br />
+              간편한 검색으로 고품질 이미지를 찾아보세요.
+            </span>
 
-      {/* 인터 */}
+            {/* 검색창 UI 부분 */}
+            <SearchBar />
+          </div>
+        </div>
+        {/* */}
+      </div>
+
+      {/* 공통 푸터 UI 부분 */}
     </div>
   );
 }
