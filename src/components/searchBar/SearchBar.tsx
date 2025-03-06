59fcef5114db35fd4a153eb8487afd2a55d34e1e
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-06 09:36:19 +0900

    [feat]: 검색 기능 추가 (텍스트 입력, 검색 버튼, 엔터 키 지원) 및 예외 처리 (입력값 없음 시 알림)

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,12 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./SearchBar.module.scss";
+import { useRecoilState, useRecoilValueLoadable } from "recoil";
+import { searchState } from "@/recoil/atoms/searchState";
+import { pageState } from "@/recoil/atoms/pageState";
+import { imageData } from "@/recoil/selectors/imageSelectors";
 
 function SearchBar() {
+  const [, setSearch] = useRecoilState(searchState);
+  const [, setPage] = useRecoilState(pageState);
+  // 검색 결과 가져오기
+  const imagesLoadable = useRecoilValueLoadable(imageData);
+
   // 검색 텍스트
   const [text, setText] = useState("");
+  // 검색 타이핑
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+  // 검색
+  const handleSearch = () => {
+    const query = text.trim() === "" ? "Korea" : text;
+    setSearch(query);
+    setPage(1);
+  };
+  // 검색 버튼
+  const onSearch = () => {
+    handleSearch();
+  };
+  // 엔터키
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
 
-  // 검색 기능
-  const onSearch = () => {};
+  // 토스트 메시지 실행 제어
+  useEffect(() => {
+    if (
+      imagesLoadable.state === "hasValue" &&
+      imagesLoadable.contents.results.length === 0
+    ) {
+      alert("검색 결과가 없습니다. 😢");
+    }
+  }, [imagesLoadable]); // imagesLoadable이 변경될 때 실행
 
   return (
     <div className={styles.searchBar}>
@@ -16,6 +52,8 @@ function SearchBar() {
           placeholder="찾으실 이미지를 검색하세요."
           className={styles.searchBar__search__input}
           value={text}
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
         />
         <img src="src/assets/icons/icon-search.svg" alt="" onClick={onSearch} />
       </div>
