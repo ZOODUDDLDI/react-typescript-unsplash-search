cc178b0180e721db09384461e78f7a5d0e143a23
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-08 17:42:12 +0900

    [feat]: 페이지 이동 기능 추가 (메인, 북마크)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 function Header() {
+  const navigate = useNavigate();
+
+  // 페이지 이동
+  const moveToPage = (filter: string) => {
+    if (filter === "main") navigate("/");
+    if (filter === "bookmark") navigate("/bookmark");
+  };
+
   return (
     <div className={styles.header}>
       {/* 로고 박스 */}
@@ -9,12 +18,22 @@ function Header() {
           src="src/assets/images/logo.png"
           alt="logo"
           className={styles.header__logoBox__logo}
+          onClick={() => {
+            moveToPage("main");
+          }}
         />
         <span className={styles.header__logoBox__title}>SnapSplash</span>
       </div>
       {/* 프로필 박스 */}
       <div className={styles.header__profileBox}>
-        <button className={styles.header__profileBox__button}>북마크</button>
+        <button
+          className={styles.header__profileBox__button}
+          onClick={() => {
+            moveToPage("bookmark");
+          }}
+        >
+          북마크
+        </button>
         <span className={styles.header__profileBox__user}>
           한주영 | HANJUYOUNG
         </span>
