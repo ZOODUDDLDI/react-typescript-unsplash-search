7413405966e748152a321ba41bdf4f9a7ca1b5f7
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-09 09:34:44 +0900

    [feat]: 북마크 기능 추가 및 북마크 페이지 구현, 컴포넌트 카드 구현, 스타일 적용

--- src/components/dialog/CardDetialDialog.tsx
+++ src/components/dialog/CardDetialDialog.tsx
@@ -1,6 +1,13 @@
 import { CardDTO } from "@/pages/mainpage/types/card";
-
+import { useEffect, useState } from "react";
 import styles from "./CardDetialDialog.module.scss";
+import toast, { toastConfig } from "react-simple-toasts";
+import "react-simple-toasts/dist/style.css";
+import "react-simple-toasts/dist/theme/dark.css";
+
+toastConfig({
+  theme: "dark",
+});
 
 interface Props {
   data: CardDTO;
@@ -8,15 +15,60 @@ interface Props {
 }
 
 function CardDetialDialog({ data, handleDialog }: Props) {
+  const [bookmark, setBookmark] = useState(false);
   // 다이얼로그 닫기
-  const closeDialog = () => {
+  const closeDialog = (event) => {
     handleDialog(false);
   };
 
-  // 북마크 기능
-  const addBookmark = () => {
-    console.log("북마크 ");
+  // 북마크 추가 이벤트
+  const addBookmark = (selected: CardDTO) => {
+    // setBookmark(!bookmark);
+    setBookmark(true);
+    const getLocalStorage = JSON.parse(localStorage.getItem("bookmark"));
+    // 1. 로컬스토리지에 bookmark이라는 데이터가 없을 경우
+    if (!getLocalStorage || getLocalStorage === null) {
+      localStorage.setItem("bookmark", JSON.stringify([selected]));
+      toast("해당 이미지를 북마크에 저장하였습니다. 😄");
+    } else {
+      // 2. 해당 이미지가 이미 로컬스토리지 bookmark라는 데이터에 저장되어 있을 경우
+      if (
+        getLocalStorage.findIndex((item: CardDTO) => item.id === selected.id) >
+        -1
+      ) {
+        toast("해당 이미지는 이미 북마크에 추가된 상태입니다. ❌");
+      } else {
+        // 3. 해당 이미지가 로컬스토리지 bookmark라는 데이터에 저장되어 있지 않을 경우 + bookmark라는 데이터에 이미 어떤 값이 담겨 있는 경우
+        const res = [...getLocalStorage];
+        res.push(selected);
+        localStorage.setItem("bookmark", JSON.stringify(res));
+
+        toast("해당 이미지를 북마크에 저장하였습니다. 😄");
+      }
+    }
   };
+
+  // 북마크 활성화
+  useEffect(() => {
+    const getLocalStorage = JSON.parse(localStorage.getItem("bookmark"));
+    if (
+      getLocalStorage &&
+      getLocalStorage.findIndex((item: CardDTO) => item.id === data.id) > -1
+    ) {
+      setBookmark(true);
+    } else if (!getLocalStorage) return;
+
+    // ESC Key 입력시, 다이얼로그 닫기
+    const escKeyDownCloseDialog = (event: any) => {
+      console.log("함수호출");
+      if (event.key === "Escape") {
+        closeDialog(event);
+      }
+    };
+
+    window.addEventListener("keydown", escKeyDownCloseDialog); // 위에 만들어 놓은 escKeyDownCloseDialog를 keydown했을 때, 이벤트로 등록한다.
+    return () => window.removeEventListener("keydown", escKeyDownCloseDialog);
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -42,10 +94,10 @@ function CardDetialDialog({ data, handleDialog }: Props) {
           <div className={styles.bookmark}>
             <button
               className={styles.bookmark__button}
-              onClick={() => addBookmark()}
+              onClick={() => addBookmark(data)}
             >
               {/* 구글 아이콘을 사용. */}
-              {/* {bookmark === false ? (
+              {bookmark === false ? (
                 <span
                   className="material-symbols-outlined"
                   style={{ fontSize: 16 + "px" }}
@@ -59,9 +111,10 @@ function CardDetialDialog({ data, handleDialog }: Props) {
                 >
                   favorite
                 </span>
-              )} */}
+              )}
               북마크
             </button>
+            <button className={styles.bookmark__button}>다운로드</button>
           </div>
         </div>
         <div className={styles.container__dialog__body}>
--- src/pages/bookmark/index.tsx
+++ src/pages/bookmark/index.tsx
@@ -1,5 +1,42 @@
-function index() {
-  return <div>북마크 페이지</div>;
+import Header from "@/components/header/Header";
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
+import { CardDTO } from "../mainpage/types/card";
+
+import styles from "./index.module.scss";
+
+function Index() {
+  const [data, setData] = useState<CardDTO[]>([]); // 북마크 데이터를 저장하는 상태
+
+  // 북마크 데이터 가져오기
+  const getData = () => {
+    const getLocalStorage = JSON.parse(localStorage.getItem("bookmark")); // 북마크 데이터 가져오기
+    if (getLocalStorage !== null) setData(getLocalStorage);
+    else setData([]);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      {/* 공통 헤더 UI 부분 */}
+      <Header />
+      <main className={styles.page__contents}>
+        {/* 만약, 데이터가 없을때 */}
+        {data.length === 0 ? (
+          <div className={styles.page__contents__noData}>
+            조회 가능한 데이터가 없습니다.
+          </div>
+        ) : (
+          data.map((item: CardDTO) => {
+            return <Card prop={item} key={item.id} />;
+          })
+        )}
+      </main>
+    </div>
+  );
 }
 
-export default index;
+export default Index;
